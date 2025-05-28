48bc6f683b9819c1c6ce9778d864883a20af4564
German Arce <germanalexis999@gmail.com>
2025-05-28 00:47:06 -0300

    add CrateTree and ask1

--- Estrategia.cs
+++ Estrategia.cs
@@ -12,9 +12,35 @@ namespace tpfinal
 
 		public String Consulta1(ArbolBinario<DecisionData> arbol)
 		{
-			string resutl = "Implementar";
-			return resutl;
-		}
+            
+            HashSet<string> predicciones = new HashSet<string>();
+
+            void Recorrer(ArbolBinario<DecisionData> nodo)
+            {
+                if (nodo == null) return;
+
+                if (nodo.esHoja())
+                {
+                    var hojaPred = nodo.getDatoRaiz().Predictions;
+                    if (hojaPred != null)
+                    {
+                        foreach (var clave in hojaPred.Keys)
+                        {
+                            predicciones.Add(clave);
+                        }
+                    }
+                }
+                else
+                {
+                    Recorrer(nodo.getHijoIzquierdo());
+                    Recorrer(nodo.getHijoDerecho());
+                }
+            }
+
+            Recorrer(arbol);
+
+            return "Posibles predicciones: " + string.Join(", ", predicciones);
+        }
 
 
 		public String Consulta2(ArbolBinario<DecisionData> arbol)
@@ -32,8 +58,29 @@ namespace tpfinal
 
 		public ArbolBinario<DecisionData> CrearArbol(Clasificador clasificador)
 		{
+            if (clasificador.crearHoja())
+            {
+               
+                var predicciones = clasificador.obtenerDatoHoja();
+                var hoja = new DecisionData(predicciones);
+                return new ArbolBinario<DecisionData>(hoja);
+            }
+            else
+            {
+                
+                var pregunta = clasificador.obtenerPregunta();
+                var nodo = new DecisionData(pregunta);
 
-			return null;
-		}
+                var clasificadorIzq = clasificador.obtenerClasificadorIzquierdo();
+                var clasificadorDer = clasificador.obtenerClasificadorDerecho();
+
+                var arbol = new ArbolBinario<DecisionData>(nodo);
+
+                arbol.agregarHijoIzquierdo(CrearArbol(clasificadorIzq));
+                arbol.agregarHijoDerecho(CrearArbol(clasificadorDer));
+
+                return arbol;
+            }
+        }
 	}
 }
