d24d90bfc1c4b87b27fde9d06a539fd8171e3a63
German Arce <germanalexis999@gmail.com>
2025-05-27 21:21:51 -0300

    inital commit

---
+++ Estrategia.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using tp1;
+using tp2;
+
+namespace tpfinal
+{
+
+	class Estrategia
+	{
+
+		public String Consulta1(ArbolBinario<DecisionData> arbol)
+		{
+			string resutl = "Implementar";
+			return resutl;
+		}
+
+
+		public String Consulta2(ArbolBinario<DecisionData> arbol)
+		{
+
+			return "Implementar";
+		}
+
+
+		public String Consulta3(ArbolBinario<DecisionData> arbol)
+		{
+			string result = "Implementar";
+			return result;
+		}
+
+		public ArbolBinario<DecisionData> CrearArbol(Clasificador clasificador)
+		{
+
+			return null;
+		}
+	}
+}
